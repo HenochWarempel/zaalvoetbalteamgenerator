02942d97d6c8786b32ede4c206cfdeb74bda97d4
HenochWarempel <143548932+HenochWarempel@users.noreply.github.com>
2024-09-13 16:31:58 +0200

    Nieuwe spelers (09-24)

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const generateAgainButton = document.getElementById('generateAgain');
 
 const players = [
     { name: 'Henoch', value: 8 },
-    { name: 'Jacqueline', value: 7 },
+    { name: 'Stephan', value: 9 },
     { name: 'John', value: 7.5 },
     { name: 'Maarten', value: 7.5 },
-    { name: 'Mathew', value: 7.5 },
+    { name: 'Mathew', value: 7 },
     { name: 'Mesach', value: 7.5 },
-    { name: 'Ruben', value: 9 },
-    { name: 'Mark', value: 8 },
-    { name: 'Stephan', value: 8 },
+    { name: 'Jan', value: 8 },
+    { name: 'Jan', value: 7.5 },
+    { name: 'Mark', value: 7.5 },
     { name: 'Tom B', value: 8 },
     { name: 'Tom P', value: 7.5 },
 ];
